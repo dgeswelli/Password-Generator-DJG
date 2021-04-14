8caad4316f4491e844bf6b58f1fb0752a8ad9e23
Dylan Geswelli <dylan.geswelli@gmail.com>
2021-04-14 18:05:02 -0400

    added confirms to check character choices after building createuserpassword function

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,55 @@ var specialCharacters = "!@#$%^&*()_-+=<>?,./{}|[]~`"
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 
+function createUserPassword() {
+    var characterLength = parseInt(
+        prompt("Choose character length of password. Required to be between 8-128 in length")
+    );
+    // Checks if entered character is a number
+    if (isNaN(characterLength) === true) {
+        alert("Please select a numerical character length!");
+        return;
+    }
+    // Checks if character length is too short
+    if (characterLength < 8) {
+        alert("Character length is too short!");
+        return;
+    }
+    // Checks if character length is too short
+    if (characterLength > 128) {
+        alert("Character length is too long!");
+        return;
+    }
+    var numberChoice = confirm("Include numbers in password?"); {
+        if (numberChoice === true) {
+            passwordOptions = passwordOptions + numbers;
+        }
+        else {
+            passwordOptions = passwordOptions;
+        }
+    }
+    var specialChoice = confirm("Include special characters in password?"); {
+        if (specialChoice === true) {
+            passwordOptions = passwordOptions + specialCharacters;
+        }
+        else {
+            passwordOptions = passwordOptions;
+        }
+    }
+    var upperChoice = confirm("Include uppercase characters in password?"); {
+        if (upperChoice === true) {
+            passwordOptions = passwordOptions + uppercase;
+        }
+        else {
+            passwordOptions = passwordOptions;
+        }
+    }
+    var lowerChoice = confirm("Include lowercase characters in password?"); {
+        if (lowerChoice === true) {
+            passwordOptions = passwordOptions + lowercase;
+        }
+        else {
+            passwordOptions = passwordOptions;
+        }
+    }
+}

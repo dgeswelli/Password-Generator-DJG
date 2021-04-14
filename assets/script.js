eed3f3b67a3411f003a3213a63253480b33a6b4c
Dylan Geswelli <dylan.geswelli@gmail.com>
2021-04-14 17:21:17 -0400

    global vars created, html structured, and basic css in place

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var generatePasswordBtn = document.querySelector("#generatePassword");
+var passwordOptions = ""
+var numbers = "1234567890";
+var specialCharacters = "!@#$%^&*()_-+=<>?,./{}|[]~`"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+

babfbabd6ea2ac8b518f10338450ca1a4d6992b3
Dylan Geswelli <dylan.geswelli@gmail.com>
2021-04-14 20:33:17 -0400

    mostly completed fixed minor issues

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-var generatePasswordBtn = document.querySelector("#generatePassword");
 var passwordOptions = ""
 var numbers = "1234567890";
-var specialCharacters = "!@#$%^&*()_-+=<>?,./{}|[]~`"
+var special = "!@#$%^&*()_-+=<>?,./{}|[]~`";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var generatePasswordBtn = document.querySelector("#generatePassword");
 
 function createUserPassword() {
     var characterLength = parseInt(
@@ -24,6 +24,7 @@ function createUserPassword() {
         alert("Character length is too long!");
         return;
     }
+    // Confirming password character options via 4 variables
     var numberChoice = confirm("Include numbers in password?"); {
         if (numberChoice === true) {
             passwordOptions = passwordOptions + numbers;
@@ -31,14 +32,16 @@ function createUserPassword() {
         else {
             passwordOptions = passwordOptions;
         }
+        console.log(numberChoice)
     }
     var specialChoice = confirm("Include special characters in password?"); {
         if (specialChoice === true) {
-            passwordOptions = passwordOptions + specialCharacters;
+            passwordOptions = passwordOptions + special;
         }
         else {
             passwordOptions = passwordOptions;
         }
+        console.log(specialChoice)
     }
     var upperChoice = confirm("Include uppercase characters in password?"); {
         if (upperChoice === true) {
@@ -47,6 +50,7 @@ function createUserPassword() {
         else {
             passwordOptions = passwordOptions;
         }
+        console.log(upperChoice)
     }
     var lowerChoice = confirm("Include lowercase characters in password?"); {
         if (lowerChoice === true) {
@@ -55,5 +59,30 @@ function createUserPassword() {
         else {
             passwordOptions = passwordOptions;
         }
+        console.log(lowerChoice)
     }
+    
+    if (!numberChoice && !specialChoice && !lowerChoice && !upperChoice) {
+        alert("Generated Password must contain at least 1 of the previous choices");
+        return }
+        else {
+            var generateUniquePassword = "";
+
+            for (var i = 0; i < characterLength; i++) {
+                generateUniquePassword += passwordOptions [Math.floor(Math.random() * passwordOptions.length)];
+            }
+            return generateUniquePassword;
+        }
+    }
+
+
+// function to display a generated password
+
+function displayUserPassword() {
+    var newPassword = createUserPassword();
+
+    var passwordComplete = document.getElementById("generatedPassword")
+    passwordComplete.value = newPassword;
 }
+
+generatePasswordBtn.addEventListener("click", displayUserPassword);
